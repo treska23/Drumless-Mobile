fa6b95f94bbd53e33e0af98854ea9a8f30c65fe2
treska23 <treska23@hotmail.com>
2026-07-21 22:46:46 +0200

    Track current MainActivity for safe task foregrounding

--- Drumless.Mobile/Platforms/Android/MainActivity.cs
+++ Drumless.Mobile/Platforms/Android/MainActivity.cs
@@ -7,4 +7,21 @@ namespace Drumless.Mobile;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    internal static MainActivity? Current { get; private set; }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        Current = this;
+        base.OnCreate(savedInstanceState);
+    }
+
+    protected override void OnDestroy()
+    {
+        if (ReferenceEquals(Current, this))
+        {
+            Current = null;
+        }
+
+        base.OnDestroy();
+    }
 }
